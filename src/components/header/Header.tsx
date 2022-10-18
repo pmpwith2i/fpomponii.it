7db0701d4c00e2d585077993b157bf904ce650db
pmpwith2i <federico.pomponii@gmail.com>
2022-10-17 18:49:52 -0700

    feat: adding dinamyc logo in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,10 +4,9 @@ import { useRouter } from 'next/router';
 import { Popover, Transition } from '@headlessui/react';
 import clsx from 'clsx';
 
+import avatarImage from '@/images/avatar.jpg';
 import { Fragment, useEffect, useRef } from 'react';
 import { Container } from '@/components/container/Container';
-import avatarImage from '@/images/avatar.jpg';
-import { StyleProps, TypeWithChildren } from '@/types/common';
 
 const CloseIcon = (props) => (
   <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
@@ -200,25 +199,22 @@ function clamp(number, a, b) {
   return Math.min(Math.max(number, min), max);
 }
 
-const AvatarContainer = ({ children, className, style }: TypeWithChildren & StyleProps) => (
+const AvatarContainer = ({ className, ...props }: any) => (
   <div
     className={clsx(
       className,
       'h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10',
     )}
-    style={style}
-  >
-    {children}
-  </div>
+    {...props}
+  />
 );
 
-const Avatar = ({ large = false, className }: StyleProps & {
-  large?: boolean;
-}) => (
+const Avatar = ({ large = false, className, ...props }: any) => (
   <Link
     href="/"
     aria-label="Home"
     className={clsx(className, 'pointer-events-auto')}
+    {...props}
   >
     <Image
       src={avatarImage}
@@ -253,7 +249,6 @@ export const Header = () => {
     }
 
     function updateHeaderStyles() {
-      if (!headerRef?.current) return;
       const { top, height } = headerRef.current.getBoundingClientRect();
       const scrollY = clamp(
         window.scrollY,
@@ -329,10 +324,10 @@ export const Header = () => {
 
     updateStyles();
     window.addEventListener('scroll', updateStyles, { passive: true });
-    window.addEventListener('resize', updateStyles, { passive: true } as any);
+    window.addEventListener('resize', updateStyles);
 
     return () => {
-      window.removeEventListener('scroll', updateStyles);
+      window.removeEventListener('scroll', updateStyles, { passive: true } as any);
       window.removeEventListener('resize', updateStyles);
     };
   }, [isHomePage]);
@@ -347,36 +342,36 @@ export const Header = () => {
         }}
       >
         {isHomePage && (
-        <>
-          <div
-            ref={avatarRef}
-            className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"
-          />
-          <Container
-            className="top-0 order-last -mb-3 pt-3"
-            style={{ position: 'var(--header-position)' }}
-          >
+          <>
             <div
-              className="top-[var(--avatar-top,theme(spacing.3))] w-full"
-              style={{ position: 'var(--header-inner-position)' } as any}
+              ref={avatarRef}
+              className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"
+            />
+            <Container
+              className="top-0 order-last -mb-3 pt-3"
+              style={{ position: 'var(--header-position)' }}
             >
-              <div className="relative">
-                <AvatarContainer
-                  className="absolute left-0 top-3 origin-left transition-opacity"
-                  style={{
-                    opacity: 'var(--avatar-border-opacity, 0)',
-                    transform: 'var(--avatar-border-transform)',
-                  }}
-                />
-                <Avatar
-                  large
-                  className="block h-16 w-16 origin-left"
-                  style={{ transform: 'var(--avatar-image-transform)' }}
-                />
+              <div
+                className="top-[var(--avatar-top,theme(spacing.3))] w-full"
+                style={{ position: 'var(--header-inner-position)' } as any}
+              >
+                <div className="relative">
+                  <AvatarContainer
+                    className="absolute left-0 top-3 origin-left transition-opacity"
+                    style={{
+                      opacity: 'var(--avatar-border-opacity, 0)',
+                      transform: 'var(--avatar-border-transform)',
+                    }}
+                  />
+                  <Avatar
+                    large
+                    className="block h-16 w-16 origin-left"
+                    style={{ transform: 'var(--avatar-image-transform)' }}
+                  />
+                </div>
               </div>
-            </div>
-          </Container>
-        </>
+            </Container>
+          </>
         )}
         <div
           ref={headerRef}
@@ -389,11 +384,11 @@ export const Header = () => {
           >
             <div className="relative flex gap-4">
               <div className="flex flex-1">
-                {/* {!isHomePage && (
-                <AvatarContainer>
-                  <Avatar />
-                </AvatarContainer>
-                )} */}
+                {!isHomePage && (
+                  <AvatarContainer>
+                    <Avatar />
+                  </AvatarContainer>
+                )}
               </div>
               <div className="flex flex-1 justify-end md:justify-center">
                 <MobileNavigation className="pointer-events-auto md:hidden" />
@@ -409,7 +404,6 @@ export const Header = () => {
         </div>
       </header>
       {isHomePage && <div style={{ height: 'var(--content-offset)' }} />}
-
     </>
   );
 };

936b96abbdd2cb739c230f8d4932fc572c34d4de
pmpwith2i <federico.pomponii@gmail.com>
2022-10-18 11:39:50 -0700

    feat: set color to blue

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ import { OuterContainer, InnerContainer } from '@/components/container/Container
 const NavLink = ({ href, children }: {href: string, children: ReactNode | ReactNode[]}) => (
   <Link
     href={href}
-    className="transition hover:text-teal-500 dark:hover:text-teal-400"
+    className="transition hover:text-blue-500 dark:hover:text-blue-400"
   >
     {children}
   </Link>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -26,9 +26,9 @@ const SocialLink = ({
   <li className={clsx(className, 'flex')}>
     <Link
       href={href}
-      className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      className="group flex text-sm font-medium text-zinc-800 transition hover:text-blue-500 dark:text-zinc-200 dark:hover:text-blue-500"
     >
-      <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+      <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-blue-500" />
       <span className="ml-4">{children}</span>
     </Link>
   </li>

9d20917be51b068d92535c5c76e3148a2f28c6fc
pmpwith2i <federico.pomponii@gmail.com>
2022-10-18 19:09:38 -0700

    fix: fix footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -21,6 +21,7 @@ export const Footer = () => (
             <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
               <NavLink href="/">Home</NavLink>
               <NavLink href="/about">About</NavLink>
+              <NavLink href="/articles">Articles</NavLink>
               {/* <NavLink href="/projects">Projects</NavLink>
               <NavLink href="/speaking">Speaking</NavLink>
               <NavLink href="/uses">Uses</NavLink> */}
